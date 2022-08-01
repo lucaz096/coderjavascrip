011b75264f70928fcecc43d1eccc0b4a94443c37
y <lucasnr28@gmail.com>
2022-08-01 00:46:56 -0300

    preparando el trabajo entregable n2

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+function saludar() {
+    console.log("¡Bienvenidos a Moda NR!");
+}
+
+function solicitarNombre(){
+    let nombreIngresado   = prompt("Ingresar nombre");
+    alert("El nombre ingresado es " + nombreIngresado);
+} 
+
+function entrada() {
+    let cliente;
+
+    let bienvenida;
+
+    let opciones;
+
+    let compra;
+
+    let menu;
+
+    let cable = 1500, madera = 1100, ladrillos = 1300, chapa = 1000, pintura = 1100,
+
+                tornillos = 1100, casco = 1500, pala = 1300, pico = 1600;
+
+            let cantidad;
+
+            let total;
+
+            cliente = prompt("Hola por favor ingrese su nombre");
+
+            bienvenida = cliente + "!!! bienvenido a Moda NR ";
+
+            console.log(bienvenida);
+
+}
