02be9a6c1e7a24612770f3492bd3b03622814a04
y <lucasnr28@gmail.com>
2022-08-01 22:39:53 -0300

    entregable nrodos

--- js/main.js
+++ js/main.js
@@ -1,36 +1,80 @@
 
-function saludar() {
-    console.log("¡Bienvenidos a Moda NR!");
-}
+let ingreseNombre
 
 function solicitarNombre(){
-    let nombreIngresado   = prompt("Ingresar nombre");
-    alert("El nombre ingresado es " + nombreIngresado);
-} 
-
-function entrada() {
     let cliente;
 
     let bienvenida;
 
-    let opciones;
+    bienvenida = cliente + "!!! bienvenido a Moda NR ";
 
-    let compra;
+    console.log(bienvenida);
+    let ingreseNombre = prompt("Ingresar su nombre");
+    console.log("Su nombre es: " + ingreseNombre);
+    return ingreseNombre; 
+} 
+function solicitarEmail(){
+    let ingreseEmail = prompt("Ingrese su Email");
+    console.log("Su mail es: " + ingreseEmail);
+    return ingreseEmail;
+}
+function ropaTemporada(){
+let invierno;
+let verano;
+opciones = parseInt(prompt("1-verano/n2-invierno/n3-Salir"));
+while (opciones != "3") {
+        switch(opciones) {
+            case 1:
+                console.log(`invierno`);
+                break;
 
-    let menu;
+            case 2:
+                console.log(`verano`);
+                break;
 
-    let cable = 1500, madera = 1100, ladrillos = 1300, chapa = 1000, pintura = 1100,
-
-                tornillos = 1100, casco = 1500, pala = 1300, pico = 1600;
-
-            let cantidad;
-
-            let total;
-
-            cliente = prompt("Hola por favor ingrese su nombre");
-
-            bienvenida = cliente + "!!! bienvenido a Moda NR ";
-
-            console.log(bienvenida);
+             case 3:
+                console.log("Salir");
+                break;
+    
+            default:
+                console.log("Ingrese por favor un numero de la lista");
+          
+}
+opciones = parseInt(prompt("1-verano/n2-invierno/n3-Salir"));
+}
 
 }
+function probarCodigo(){
+
+    let opciones;
+
+    let buzo = 1500, campera = 1100, pantalones = 1300, remeras = 1000, camisas = 1100;
+
+    opciones = parseInt(prompt("Ahora podras ver nuestro catalogo\n1-buzo\n2-campera\n3-pantalones\n4. Salir"));
+
+
+while (opciones != "4") {
+        switch (opciones) {
+            case 1:
+                console.log(`buzo ${buzo}`);
+                break;
+
+            case 2:
+                console.log(`camisas ${camisas}`);
+                break;
+
+            case 3:
+                console.log(`pantalones ${pantalones}`);
+                break;
+                
+            case 4:
+                console.log("Salir");
+                break;
+
+            default:
+                console.log("Ingrese por favor un numero de la lista");
+        }
+        opciones = parseInt(prompt("1-pantalones\n2-campera\n3-buzo\n4-Salir"));
+    }
+
+  }
