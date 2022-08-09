15e9387cf786c077871fab7aefd23315f337c272
y <lucasnr28@gmail.com>
2022-08-08 22:26:19 -0300

    desafio entregable

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
-let ingreseNombre
-
+/*let ingreseNombre
 function solicitarNombre(){
     let cliente;
 
@@ -12,13 +11,27 @@ function solicitarNombre(){
     let ingreseNombre = prompt("Ingresar su nombre");
     console.log("Su nombre es: " + ingreseNombre);
     return ingreseNombre; 
-} 
+} */
+
+function solicitarNombre() {
+
+    const listaNombres = [];
+    let   cantidad     = 1;
+    do{
+       let entrada = prompt("Ingresar nombre");
+       listaNombres.push(entrada.toUpperCase());
+       console.log(listaNombres.length);
+    }while(listaNombres.length != cantidad)
+    const nuevaLista = listaNombres.concat(["LUCAS","MANUEL"]);
+
+    alert(nuevaLista.join("\n"));
+}
 function solicitarEmail(){
     let ingreseEmail = prompt("Ingrese su Email");
     console.log("Su mail es: " + ingreseEmail);
     return ingreseEmail;
 }
-function ropaTemporada(){
+/*function ropaTemporada(){
 let invierno;
 let verano;
 opciones = parseInt(prompt("1-verano/n2-invierno/n3-Salir"));
@@ -44,6 +57,7 @@ opciones = parseInt(prompt("1-verano/n2-invierno/n3-Salir"));
 }
 
 }
+
 function probarCodigo(){
 
     let opciones;
@@ -78,3 +92,94 @@ while (opciones != "4") {
     }
 
   }
+  */
+
+
+  function CrearListado()
+  {
+      
+      const ListadoProductos = [];
+      ListadoProductos.push(new Producto("buzo", "1000",23));
+      ListadoProductos.push(new Producto("campera", "1500",12));
+      ListadoProductos.push(new Producto("pantalon", "900",4));
+  
+    
+     ListadoProductos[1].VenderPorCantidad(3) ;
+    
+      for (const elementoTipoProducto of ListadoProductos)
+      {
+          elementoTipoProducto.MostrarProducto();
+          console.log(elementoTipoProducto.nombre);
+          elementoTipoProducto.vendido=false;
+           
+      }
+  
+      let cantidadParaComprar=prompt("ingrese cantida");
+      cantidadParaComprar =parseInt(cantidadParaComprar );
+  
+      let productoParaComprar=prompt("Ingrese producto");
+      productoParaComprar =productoParaComprar.toUpperCase();
+  
+      for (const elementoTipoProducto of ListadoProductos)
+      {
+           if(elementoTipoProducto.nombre == productoParaComprar) 
+           {
+              elementoTipoProducto.VenderPorCantidad(cantidadParaComprar);
+           }    
+      }
+  }
+  
+  class Cliente {
+  
+      constructor(nombre, edad,documento) {
+          this.nombre  = nombre.toUpperCase();
+          this.edad  = parseFloat(edad);
+          this.documento = documento;
+      }
+      
+  
+    
+      MostrarCliente() {
+          console.log("nombre: "+this.nombre +" legajo: "+this.documento) ;
+      }
+  }
+  
+
+  class Producto {
+
+    constructor(nombre, precio,stock) {
+        this.nombre  = nombre.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.vendido = false;
+        this.stock = stock;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+
+    VenderPorCantidad(cantidad) {
+        if(cantidad > this.stock )
+        {
+            console.log("no hay stock");
+        }
+        else
+        {
+            this.stock  = this.stock-cantidad;
+            this.vendido = true;
+        }
+       
+    }
+    MostrarProducto() {
+        
+        console.log("nombre: "+this.nombre +" precio: "+this.precio) ;
+        if(this.vendido==true)
+        {
+            console.log("Se vendio");
+        }
+        else
+        {
+             console.log("No se vendio");
+        }
+    }
+
+}
